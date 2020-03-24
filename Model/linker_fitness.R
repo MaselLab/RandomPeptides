87a2633e69b5c4493ed54a18d1e198772850b33d
Luke Kosinski <46657629+lkosin@users.noreply.github.com>
2020-03-23 21:05:16 -0700

    Update linker_fitness.R

--- Model/linker_fitness.R
+++ Model/linker_fitness.R
@@ -16,7 +16,7 @@ bc.back <- function(data.transformed, lambda){
 }
 
 # Global variables for quick editing.
-todays.date <- "3-17-2020"
+todays.date <- "3-23-2020"
 cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 # Load Frumkin et al. data, kindly sent to us by Dvir Schirman.
@@ -41,6 +41,7 @@ ggplot(
   geom_smooth()
 
 # Looking at the distributions.
+quantile(frumkin.data$fitness_residuals_mean)
 hist(frumkin.data$fitness_residuals_mean)
 fit.resid.min.1 <- 1 - min(frumkin.data$fitness_residuals_mean)
 hist(frumkin.data$fitness_residuals_mean + fit.resid.min.1)
@@ -53,14 +54,41 @@ boxcox(frumkin.data$Predicted.fitness ~ 1)
 # Lambda = 0
 hist(log(frumkin.data$Predicted.fitness))
 
+ggplot(
+  data = frumkin.data,
+  aes(
+    x = Predicted.fitness,
+    y = fitness_residuals_mean
+  )
+) +
+  geom_point() +
+  geom_smooth(method = "loess", color = "red") +
+  geom_smooth(method = "lm") +
+  ylab("Fitness residuals") +
+  xlab("Predicted fitness") +
+  theme_bw(base_size = 28)
+
+# Making a better histogram of transformed mean fitness residuals.
+ggplot(
+  data = frumkin.data,
+  aes(
+    x = bc.transform(frumkin.data$fitness_residuals_mean + fit.resid.min.1, lambda = fit.resid.lambda)
+  )
+) +
+  geom_histogram(bins = 11) +
+  xlab("Fitness residuals") +
+  scale_x_continuous(breaks = bc.transform(c(-0.01, 0, 0.002) + fit.resid.min.1, lambda = 250),
+                     labels = c(-0.01, 0, 0.002)) +
+  theme_bw(base_size = 28)
+
 # Checking the relationship with predicted fitness.
 path.title.plot <- paste("Scripts/Figures/linker_fitness_predicted_", todays.date, ".png", sep = "")
 png(filename = path.title.plot, width = 500, height = 500)
 ggplot(
   data = frumkin.data,
   aes(
-    x = log(Predicted.fitness),
-    y = bc.transform(frumkin.data$fitness_residuals_mean + fit.resid.min.1, lambda = fit.resid.lambda)
+    x = Predicted.fitness,
+    y = fitness_residuals_mean
   )
 ) +
   geom_point() +
@@ -68,18 +96,21 @@ ggplot(
   geom_smooth(method = "lm", color = cbbPalette[6]) +
   xlab("Predicted fitness") +
   ylab("Fitness residual") +
-  scale_y_continuous(breaks = bc.transform(c(-0.01, 0, 0.002) + fit.resid.min.1, lambda = 250),
-                     labels = c(-0.01, 0, 0.002)) +
-  scale_x_continuous(breaks = log(c(0.05, 0.15, 0.4)),
-                     labels = c(0.05, 0.15, 0.4)) +
+  # scale_y_continuous(breaks = bc.transform(c(-0.01, 0, 0.002) + fit.resid.min.1, lambda = 250),
+  #                    labels = c(-0.01, 0, 0.002)) +
+  # scale_x_continuous(breaks = log(c(0.05, 0.15, 0.4)),
+  #                    labels = c(0.05, 0.15, 0.4)) +
   theme_bw(base_size = 28)
 dev.off()
 
 # Building a simple linear regression model.
 linker.lm <- lm(
   data = frumkin.data,
-  formula = I(bc.transform(frumkin.data$fitness_residuals_mean + fit.resid.min.1, lambda = 250)) ~ log(Predicted.fitness)
+  formula = frumkin.data$fitness_residuals_mean ~ Predicted.fitness
 )
 summary(linker.lm)
 plot(linker.lm)
 drop1(linker.lm, test = "Chisq")
+
+# Spearman's correlation.
+cor.test(frumkin.data$fitness_residuals_mean, frumkin.data$Predicted.fitness, method = "spearman")
